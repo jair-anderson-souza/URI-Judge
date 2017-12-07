f38342f898e5f0c4d702bfe572803d63e115e87f
Anderson Souza <jair_anderson_bs@hotmail.com>
2017-12-07 00:58:35 -0300

    question 1022

--- 1021.c
+++ 1021.c
@@ -1,67 +1,66 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 
 #define CINQ 0.5
 #define VINT_C 0.25
 
 int main(int argc, char const *argv[])
-{
-	int valor = 0;
-	scanf("%d", &valor);
+{	
+	float valor = 0;
+	scanf("%f", &valor);
 
 	if (0 <= valor && valor <= 1000000.00)
 	{
 		printf("NOTAS:\n");
 		int cem = valor / 100;
-		int resto = valor % 100;
+		float resto = fmod(valor, 100);
 		printf("%d nota(s) de R$ 100.00\n", cem);
 		
 		int cinq = resto / 50;
-		resto = resto % 50;
+		resto = fmod(resto, 50);
 		printf("%d nota(s) de R$ 50.00\n", cinq);
 
 		int vinte = resto / 20;
-		resto = resto % 20;
-		printf("%d nota(s) de R$ 2.00\n", vinte);
+		resto = fmod(resto, 20);
+		printf("%d nota(s) de R$ 20.00\n", vinte);
 		
 		int dez = resto / 10;
-		resto = resto % 10;
+		resto = fmod(resto, 10);
 		printf("%d nota(s) de R$ 10.00\n", dez);
 
 		int cinco = resto / 5;
-		resto = resto % 5;
+		resto = fmod(resto, 5);
 		printf("%d nota(s) de R$ 5.00\n", cinco);
 
 		int dois = resto / 2;
-		resto = resto % 2;
+		resto = fmod(resto, 2);
 		printf("%d nota(s) de R$ 2.00\n", dois);
 
 		printf("MOEDAS:\n");
-		int um = resto;
-		//resto = resto % 1;
+		int um = resto / 1;
+		resto = fmod(resto, 1);
 		printf("%d moeda(s) de R$ 1.00\n", um);
 	
 		int cinq_m = resto / 0.5;
-		int resto_2 = resto * CINQ;
-		printf("%d moeda(s) de R$ 0.50\n", um);
+		resto = fmod(resto, 0.5);
+		printf("%d moeda(s) de R$ 0.50\n", cinq_m);
 
 		int vinte_m = resto / 0.25; 
-		resto = resto * 0.25;
-		printf("%d moeda(s) de R$ 0.25\n", um);
+		resto = fmod(resto, 0.25);
+		printf("%d moeda(s) de R$ 0.25\n", vinte_m);
 
 		int dez_m = resto / 0.1;
-		resto = resto % 0.1;
-		printf("%d moeda(s) de R$ 0.10\n", um);
+		resto = fmod(resto, 0.1);
+		printf("%d moeda(s) de R$ 0.10\n", dez_m);
 
 		int cinco_m = resto / 0.05;
-		resto = resto * 0.05;
-		printf("%d moeda(s) de R$ 0.05\n", um);
+		resto = fmod(resto, 0.05);
+		printf("%d moeda(s) de R$ 0.05\n", cinco_m);
 
-		int um_c = resto / 0.01;
-		resto = resto * 0.01;
-
-		printf("%d moeda(s) de R$ 0.01\n", um);
-
+		double um_c = resto * 100;
+		resto = fmod(resto, 0.01);
+		printf("%.0lf moeda(s) de R$ 0.01\n", um_c);
 	}
 	return 0;
 }
